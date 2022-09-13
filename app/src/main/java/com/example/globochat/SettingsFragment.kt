e01a1017bf59720ec424ff7536e37260a1fca164
Fbada006 <ferdinandkamata@yahoo.com>
2022-09-13 16:15:31 +0300

    remove unnecessary older settings fragment

--- app/src/main/java/com/example/globochat/SettingsFragment.kt
+++ app/src/main/java/com/example/globochat/SettingsFragment.kt
@@ -9,20 +9,4 @@ import androidx.navigation.fragment.NavHostFragment
 
 class SettingsFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_settings, container, false)
-
-        view.setOnClickListener {
-            val navHostFragment =
-                activity?.supportFragmentManager?.findFragmentById(R.id.nav_host_frag) as NavHostFragment
-            val navController = navHostFragment.navController
-            val action = SettingsFragmentDirections.actionSettingsToAccSettings()
-            navController.navigate(action)
-        }
-
-        return view
-    }
 }
