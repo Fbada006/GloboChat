2f7ad99976dca9eb0f2b6dedd51fcb21701e8ca6
Fbada006 <ferdinandkamata@yahoo.com>
2022-09-13 16:17:48 +0300

    use preferences compat frag class

--- app/src/main/java/com/example/globochat/SettingsFragment.kt
+++ app/src/main/java/com/example/globochat/SettingsFragment.kt
@@ -1,12 +1,11 @@
 package com.example.globochat
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.NavHostFragment
+import androidx.preference.PreferenceFragmentCompat
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
 
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.settings, rootKey)
+    }
 }
