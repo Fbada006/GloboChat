6eae72c78a4f5d4a97fc276efd3dd8e2c312ea7a
Fbada006 <ferdinandkamata@yahoo.com>
2022-09-13 15:59:09 +0300

    add project files

--- app/src/main/java/com/example/globochat/MainActivity.kt
+++ app/src/main/java/com/example/globochat/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.globochat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.globochat.R
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        // Get NavHost and NavController
+//        val navHostFrag = supportFragmentManager.findFragmentById(R.id.nav_host_frag) as NavHostFragment
+//        navController = navHostFrag.navController
+//
+//        // Get AppBarConfiguration
+//        appBarConfiguration = AppBarConfiguration(navController.graph)
+//
+//        // Link ActionBar with NavController
+//        setupActionBarWithNavController(navController, appBarConfiguration)
     }
+
+//    override fun onSupportNavigateUp(): Boolean {
+//        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+//    }
 }
---
+++ app/src/main/java/com/example/globochat/SettingsFragment.kt
@@ -0,0 +1,29 @@
+package com.example.globochat
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
+import com.example.globochat.R
+
+
+class SettingsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_settings, container, false)
+
+        view.setOnClickListener {
+            val navHostFragment = activity?.supportFragmentManager?.findFragmentById(R.id.nav_host_frag) as NavHostFragment
+            val navController = navHostFragment.navController
+            val action = SettingsFragmentDirections.actionSettingsToAccSettings()
+            navController.navigate(action)
+        }
+
+        return view
+    }
+}
